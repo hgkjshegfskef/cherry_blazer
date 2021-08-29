cce9616bf3d03b4a4ea7c261d830080974f8116d
hgkjshegfskef <jyhgekyfbkjsyebf@protonmail.ch>
2021-08-29 17:57:10 +0200

    fix code according to naming convention

--- src/main.cc
+++ src/main.cc
@@ -5,16 +5,16 @@
 #include <string>
 #include <system_error>
 
-const int canvas_width = 3;
-const int canvas_height = 2;
-const int ppm_max_val_per_color = 255;
-char const* const image_file_name = "image.ppm";
+const int CANVAS_WIDTH = 3;
+const int CANVAS_HEIGHT = 2;
+const int PPM_MAX_VAL_PER_COLOR = 255;
+char const* const IMAGE_FILE_NAME = "image.ppm";
 
 auto ppm_generate_header(int width, int height) {
     std::stringstream ss = {};
     ss << "P3\n"
        << std::to_string(width) << ' ' << std::to_string(height) << '\n'
-       << std::to_string(ppm_max_val_per_color) << '\n';
+       << std::to_string(PPM_MAX_VAL_PER_COLOR) << '\n';
     return ss.str();
 }
 
@@ -22,19 +22,19 @@ auto ppm_write_header(std::string const& file) {
     std::ofstream f(file);
     if (!f)
         throw std::system_error(errno, std::system_category(), "failed to open '" + file + "'");
-    f << ppm_generate_header(canvas_width, canvas_height);
+    f << ppm_generate_header(CANVAS_WIDTH, CANVAS_HEIGHT);
     return f;
 }
 
 int main() {
     std::ofstream image_file;
     try {
-        image_file = ppm_write_header(image_file_name);
+        image_file = ppm_write_header(IMAGE_FILE_NAME);
     } catch (std::system_error const& e) {
         std::cerr << e.what() << " (" << e.code() << ")" << std::endl;
     }
-    for (int i = 0; i < canvas_height; ++i) {
-        for (int j = 0; j < canvas_width; ++j) {
+    for (int i = 0; i < CANVAS_HEIGHT; ++i) {
+        for (int j = 0; j < CANVAS_WIDTH; ++j) {
             image_file << "0 0 255\n";
         }
     }
