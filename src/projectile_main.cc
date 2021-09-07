d6c750dec0e98fe8a9bfd5147874781058104e3a
hgkjshegfskef <jyhgekyfbkjsyebf@protonmail.ch>
2021-09-07 13:44:03 +0200

    plot projectile on the canvas

--- src/projectile_main.cc
+++ src/projectile_main.cc
@@ -1,29 +1,62 @@
+#include "canvas.hh"
 #include "point.hh"
 #include "projectile.hh"
 #include "vector.hh"
 
+#include <cstdlib>
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <system_error>
 
 namespace cb = cherry_blazer;
+
+namespace {
+std::ofstream open_file(std::string const& file) {
+    std::ofstream f{file};
+    if (!f)
+        throw std::system_error(errno, std::system_category(), "failed to open '" + file + "'");
+    return f;
+}
+} // namespace
 
 int main() {
     // projectile starts one unit above the origin.
     // velocity is normalized to 1 unit/tick.
-    Projectile proj{{0, 1, 0}, cb::normalize({1, 1, 0})};
+    // NOLINTNEXTLINE(readability-magic-numbers)
+    Projectile proj{cb::Point{0, 1, 0}, cb::normalize(cb::Vector{1, 1.8, 0}) * 11.25};
 
     // gravity -0.1 unit/tick, and wind is -0.01 unit/tick.
-    Environment env{{0, -0.1, 0}, {-0.01, 0, 0}}; // NOLINT(readability-magic-numbers)
+    // NOLINTNEXTLINE(readability-magic-numbers)
+    Environment env{cb::Vector{0, -0.1, 0}, cb::Vector{-0.01, 0, 0}};
+
+    cb::Canvas canvas{900, 550}; // NOLINT(readability-magic-numbers)
+    cb::Color red{1, 0, 0};
 
     int counter{};
     Projectile p = tick(env, proj);
     std::cout << "[Tick #" << ++counter << "] "
               << "Projectile position: " << p.position << std::endl;
-
     do {
+        // convert between canvas' and projectile's Y coordinates: projectile's Y coordinates go up,
+        // while canvas' Y coordinates go down
+        canvas(p.position.x, canvas.height() - p.position.y) = red;
         p = tick(env, p);
         std::cout << "[Tick #" << ++counter << "] "
                   << "Projectile position: " << p.position << std::endl;
     } while (p.position.y > 0);
 
     std::cout << "Projectile hit the ground after " << counter << " ticks." << std::endl;
+
+    std::ofstream image_file;
+    try {
+        image_file = open_file("image.ppm");
+    } catch (std::system_error const& e) {
+        std::cerr << e.what() << " (" << e.code() << ")" << std::endl;
+        throw;
+    }
+
+    const std::string image_contents = canvas.as_ppm();
+    image_file.write(image_contents.data(), image_contents.size());
 }
