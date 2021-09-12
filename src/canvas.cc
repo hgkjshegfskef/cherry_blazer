97e27345d9ace72c4980aeb42bdeda4516c59b91
hgkjshegfskef <jyhgekyfbkjsyebf@protonmail.ch>
2021-09-12 12:00:42 +0200

    use point2d for ranges

--- src/canvas.cc
+++ src/canvas.cc
@@ -1,5 +1,6 @@
 #include "canvas.hh"
 
+#include "point2d.hh"
 #include "ppm.hh"
 #include "types.hh"
 
@@ -20,20 +21,19 @@ namespace {
 // https://en.wikipedia.org/wiki/Linear_interpolation
 // If the two known points are given by the coordinates (x0,y0) and (x1,y1), the linear interpolant
 // is the straight line between these points. Value of x must be in the interval [x0;x1].
-constexpr double linear_interpolation(double x, std::pair<double, double> const& left_point,
-                                      std::pair<double, double> const& right_point) {
-    assert(left_point.first <= x && x <= right_point.first);
-    return left_point.second + (x - left_point.first) * (right_point.second - left_point.second) /
-                                   (right_point.first - left_point.first);
+constexpr double linear_interpolation(double x, Point2d const& left_point,
+                                      Point2d const& right_point) {
+    assert(left_point.x <= x && x <= right_point.x);
+    return left_point.y +
+           (x - left_point.x) * (right_point.y - left_point.y) / (right_point.x - left_point.x);
 }
 
 // Scale a number between two (possibly overlapping) ranges.
 // Use-case example: given a value in range [0;1], find out its respective value in range [0;255].
 // Further reading: https://gamedev.stackexchange.com/a/33445
-constexpr double scale(double value, std::pair<double, double> const& source_range,
-                       std::pair<double, double> const& target_range) {
-    return linear_interpolation(value, {source_range.first, target_range.first},
-                                {source_range.second, target_range.second});
+constexpr double scale(double value, Point2d const& source_range, Point2d const& target_range) {
+    return linear_interpolation(value, {source_range.x, target_range.x},
+                                {source_range.y, target_range.y});
 }
 
 } // namespace
@@ -72,30 +72,12 @@ std::string Canvas::as_ppm() const {
     const auto batch_count = size() / batch_size;
 
     // Range of canvas color components' values.
-    constexpr auto component_min{0.0};
-    constexpr auto component_max{1.0};
-    constexpr std::pair component_range{component_min, component_max};
-
-    //    constexpr Point2d component_range{component_min, component_max};
-
-    //    constexpr safe_uliteral_auto_trap<0> component_min{};
-    //    constexpr safe_uliteral_auto_trap<1> component_max{};
-    //    constexpr safe_urange_auto_trap<component_min, component_max>
-    //    component_range{component_min};
-
+    constexpr Point component_range{0.0, 1.0};
     // Range of PPM color values.
-    constexpr auto ppm_min{0.0};
-    constexpr auto ppm_max{255.0};
-    constexpr std::pair ppm_range{ppm_min, ppm_max};
-
-    //    constexpr Point2d ppm_range{ppm_min, ppm_max};
-
-    //    constexpr safe_uliteral_auto_trap<0> ppm_min{};
-    //    constexpr safe_uliteral_auto_trap<255> ppm_max{};
-    //    constexpr safe_urange_auto_trap<ppm_min, ppm_max> ppm_range{ppm_min};
+    constexpr Point ppm_range{0.0, 255.0};
 
     std::stringstream ss;
-    ss << ppm::generate_header(width_, height_, u32(ppm_max));
+    ss << ppm::generate_header(width_, height_, u32(ppm_range.y));
 
     // Print out batch_count batches of batch_size amount of colors each, one batch per line.
     for (auto nth_batch{0U}; nth_batch < batch_count; ++nth_batch) {
@@ -104,8 +86,9 @@ std::string Canvas::as_ppm() const {
             boost::pfr::for_each_field(
                 canvas_[nth_batch * batch_size + nth_color], [&](auto const& component) {
                     ss << std::setw(component_width)
-                       << std::round(scale(std::clamp(component, component_min, component_max),
-                                           component_range, ppm_range));
+                       << std::round(
+                              scale(std::clamp(component, component_range.x, component_range.y),
+                                    component_range, ppm_range));
                 });
         }
         ss << "\n";
@@ -116,7 +99,7 @@ std::string Canvas::as_ppm() const {
         for (auto leftover{already_processed}; leftover < size(); ++leftover) {
             boost::pfr::for_each_field(canvas_[leftover], [&](auto const& component) {
                 ss << std::setw(component_width)
-                   << std::round(scale(std::clamp(component, component_min, component_max),
+                   << std::round(scale(std::clamp(component, component_range.x, component_range.y),
                                        component_range, ppm_range));
             });
         }
