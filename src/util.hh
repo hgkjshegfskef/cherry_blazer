002fc7f2aacc9a53b1c9eafabf78ef06ba20d193
hgkjshegfskef <jyhgekyfbkjsyebf@protonmail.ch>
2021-09-15 23:29:59 +0200

    make almost_equal constexpr

--- src/util.hh
+++ src/util.hh
@@ -13,8 +13,8 @@ namespace cherry_blazer {
 // Taken from: https://en.cppreference.com/w/cpp/types/numeric_limits/epsilon
 // ULP=4 was chosen arbitrarily as common value.
 template <class T>
-typename std::enable_if<!std::numeric_limits<T>::is_integer, bool>::type almost_equal(T x, T y,
-                                                                                      u8 ulp = 4) {
+constexpr typename std::enable_if<!std::numeric_limits<T>::is_integer, bool>::type
+almost_equal(T x, T y, u8 ulp = 4) {
     // the machine epsilon has to be scaled to the magnitude of the values used
     // and multiplied by the desired precision in ULPs (units in the last place)
     return std::fabs(x - y) <= std::numeric_limits<T>::epsilon() * std::fabs(x + y) * ulp
