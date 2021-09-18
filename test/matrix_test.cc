3a40ab9443e3e6fd48ab7090c586d41e02970df1
hgkjshegfskef <jyhgekyfbkjsyebf@protonmail.ch>
2021-09-18 22:39:44 +0200

    matrix: add more inversion tests

--- test/matrix_test.cc
+++ test/matrix_test.cc
@@ -217,7 +217,7 @@ TEST(MatrixTest, MatrixNonInvertible) { // NOLINT
     EXPECT_EQ(result, false);
 }
 
-TEST(MatrixTest, MatrixInverse) { // NOLINT
+TEST(MatrixTest, MatrixInverse1) { // NOLINT
     CHERRY_BLAZER_CONSTEXPR Matrix mat{
         {-5., 2., 6., -8.}, {1., -5., 1., 8.}, {7., 7., -6., -7.}, {1., -3., 7., 4.}};
     ASSERT_EQ(det(mat), 532);
@@ -233,6 +233,50 @@ TEST(MatrixTest, MatrixInverse) { // NOLINT
     for (u16 i = 0; i < 4; ++i) {
         for (u16 j = 0; j < 4; ++j) {
             EXPECT_NEAR(result(i, j), expected(i, j), 1e-5) << result;
+        }
+    }
+}
+
+TEST(MatrixTest, MatrixInverse2) { // NOLINT
+    CHERRY_BLAZER_CONSTEXPR Matrix mat{
+        {8., -5., 9., 2.}, {7., 5., 6., 1.}, {-6., 0., 9., 6.}, {-3., 0., -9., -4.}};
+    CHERRY_BLAZER_CONSTEXPR auto result = inverse(mat);
+    CHERRY_BLAZER_CONSTEXPR auto expected = Mat4d{{-0.15385, -0.15385, -0.28205, -0.53846},
+                                                  {-0.07692, 0.12308, 0.02564, 0.03077},
+                                                  {0.35897, 0.35897, 0.43590, 0.92308},
+                                                  {-0.69231, -0.69231, -0.76923, -1.92308}};
+    for (u16 i = 0; i < 4; ++i) {
+        for (u16 j = 0; j < 4; ++j) {
+            EXPECT_NEAR(result(i, j), expected(i, j), 1e-5) << result;
+        }
+    }
+}
+
+TEST(MatrixTest, MatrixInverse3) { // NOLINT
+    CHERRY_BLAZER_CONSTEXPR Matrix mat{
+        {9., 3., 0., 9.}, {-5., -2., -6., -3.}, {-4., 9., 6., 4.}, {-7., 6., 6., 2.}};
+    CHERRY_BLAZER_CONSTEXPR auto result = inverse(mat);
+    CHERRY_BLAZER_CONSTEXPR auto expected = Mat4d{{-0.04074, -0.07778, 0.14444, -0.22222},
+                                                  {-0.07778, 0.03333, 0.36667, -0.33333},
+                                                  {-0.02901, -0.14630, -0.10926, 0.12963},
+                                                  {0.17778, 0.06667, -0.26667, 0.33333}};
+    for (u16 i = 0; i < 4; ++i) {
+        for (u16 j = 0; j < 4; ++j) {
+            EXPECT_NEAR(result(i, j), expected(i, j), 1e-5) << result;
+        }
+    }
+}
+
+TEST(MatrixTest, MultiplyProductByItsInverse) { // NOLINT
+    CHERRY_BLAZER_CONSTEXPR Matrix a{
+        {3., -9., 7., 3.}, {3., -8., 2., -9.}, {-4., 4., 4., 1.}, {-6., 5., -1., 1.}};
+    CHERRY_BLAZER_CONSTEXPR Matrix b{
+        {8., 2., 2., 2.}, {3., -1., 7., 0.}, {7., 0., 5., 4.}, {6., -2., 0., 5.}};
+    CHERRY_BLAZER_CONSTEXPR auto c = a * b;
+    CHERRY_BLAZER_CONSTEXPR auto result = c * inverse(b);
+    for (u16 i = 0; i < 4; ++i) {
+        for (u16 j = 0; j < 4; ++j) {
+            EXPECT_NEAR(result(i, j), a(i, j), 1e-5) << result;
         }
     }
 }
