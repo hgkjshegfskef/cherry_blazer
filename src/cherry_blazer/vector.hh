0c12d3d71c7dd2de29a1475c169aad100e0f0f90
hgkjshegfskef <jyhgekyfbkjsyebf@protonmail.ch>
2021-09-27 18:18:30 +0200

    vector: delete unused code

--- src/cherry_blazer/vector.hh
+++ src/cherry_blazer/vector.hh
@@ -18,11 +18,6 @@ class Matrix<Precision, OuterDimension, 1>
     : public detail::MatrixBase<Precision, OuterDimension + 1, 1> {
 
     using base = detail::MatrixBase<Precision, OuterDimension + 1, 1>;
-
-    // Declare size constant in the style of template parameters to match the rest of Matrix.
-    //    static inline constexpr std::size_t InnerDimension = 1; //
-    //    NOLINT(readability-identifier-naming)
-
     using typename base::impl;
 
   public:
@@ -73,20 +68,6 @@ using Vec2f = Matrix<float, 2, 1>;  // NOLINT(readability-identifier-naming)
 using Vec3f = Matrix<float, 3, 1>;  // NOLINT(readability-identifier-naming)
 using Vec2d = Matrix<double, 2, 1>; // NOLINT(readability-identifier-naming)
 using Vec3d = Matrix<double, 3, 1>; // NOLINT(readability-identifier-naming)
-
-// template <typename Precision, std::size_t OuterDimension>
-// constexpr bool operator==(Matrix<Precision, OuterDimension, 1> const& lhs,
-//                           Matrix<Precision, OuterDimension - 1, 1> const& rhs) noexcept {
-//     // TODO: almost_equal is not constexpr
-//     return std::equal(std::cbegin(lhs), std::cend(lhs), std::cbegin(rhs), std::cend(rhs),
-//                       [](auto const& lhs, auto const& rhs) { return almost_equal(lhs, rhs); });
-// }
-//
-// template <typename Precision, std::size_t OuterDimension>
-// constexpr bool operator!=(Matrix<Precision, OuterDimension, 1> const& lhs,
-//                           Matrix<Precision, OuterDimension, 1> const& rhs) noexcept {
-//     return !(lhs == rhs);
-// }
 
 // This is actually a Vector, but it cannot be named.
 extern template class Matrix<double, 3, 1>;
