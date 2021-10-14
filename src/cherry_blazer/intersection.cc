eb61aea3da435a406352c1d3ae729cbc09d116ec
hgkjshegfskef <jyhgekyfbkjsyebf@protonmail.ch>
2021-10-14 17:29:24 +0200

    intersect() still broken?

--- src/cherry_blazer/intersection.cc
+++ src/cherry_blazer/intersection.cc
@@ -17,12 +17,7 @@ namespace cherry_blazer {
 
 Intersection::Intersection(double t, Sphere const& object) : t{t}, object{object} {}
 
-// https://en.wikipedia.org/wiki/Line%E2%80%93sphere_intersection
 std::vector<Intersection> intersect(Sphere const& sphere, Ray const& ray) {
-    // TODO?: sphere center is at world origin, radius = 1
-    auto const sphere_center = Point{0., 0., 0.};
-    auto const sphere_radius = 1.;
-
     // Account for the transformations applied to sphere (so, apply the inverse of them to ray).
     auto transformed_ray = transform(
         ray, Transformation{inverse(sphere.transformation.mat), sphere.transformation.kind});
@@ -30,15 +25,12 @@ std::vector<Intersection> intersect(Sphere const& sphere, Ray const& ray) {
     transformed_ray.direction = normalize(transformed_ray.direction);
 
     // Create vector from the sphere center towards ray origin.
-    auto const from_sphere_to_transformed_ray = transformed_ray.origin - sphere_center;
+    auto const from_sphere_to_transformed_ray = transformed_ray.origin - Point{0., 0., 0.};
 
-    // Make the ray direction a unit vector (enables better formula).
-    auto const transformed_ray_direction = normalize(transformed_ray.direction);
-
-    auto const b = dot(transformed_ray_direction, from_sphere_to_transformed_ray);
-    auto const discriminant =
-        b * b - (dot(from_sphere_to_transformed_ray, from_sphere_to_transformed_ray) -
-                 sphere_radius * sphere_radius);
+    auto const a = dot(transformed_ray.direction, transformed_ray.direction);
+    auto const b = 2. * dot(transformed_ray.direction, from_sphere_to_transformed_ray);
+    auto const c = dot(from_sphere_to_transformed_ray, from_sphere_to_transformed_ray) - 1.;
+    auto const discriminant = b * b - 4. * a * c;
 
     if (discriminant < 0.)
         return {};
@@ -46,10 +38,11 @@ std::vector<Intersection> intersect(Sphere const& sphere, Ray const& ray) {
     auto const sqrt_discriminant = std::sqrt(discriminant);
 
     if (detail::almost_equal(discriminant, 0.)) {
-        return {{-b - sqrt_discriminant, sphere}};
+        return {{(-b) / (2. * a), sphere}};
     }
 
-    return {{-b - sqrt_discriminant, sphere}, {-b + sqrt_discriminant, sphere}};
+    return {{(-b - sqrt_discriminant) / (2. * a), sphere},
+            {(-b + sqrt_discriminant) / (2. * a), sphere}};
 }
 
 Intersection const* hit(std::vector<Intersection> const& intersections) {
