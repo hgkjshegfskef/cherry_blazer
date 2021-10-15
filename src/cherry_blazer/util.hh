aefd54a2b9adc37f9548ca60483d65d4366d7456
hgkjshegfskef <jyhgekyfbkjsyebf@protonmail.ch>
2021-10-15 14:44:05 +0200

    constexpr implies inline for functions
    As a result, constexpr functions must be inline.

--- src/cherry_blazer/util.hh
+++ src/cherry_blazer/util.hh
@@ -8,7 +8,11 @@ namespace cherry_blazer::util {
 // https://en.wikipedia.org/wiki/Linear_interpolation
 // If the two known points are given by the coordinates (x0,y0) and (x1,y1), the linear interpolant
 // is the straight line between these points. Value of x must be in the interval [x0;x1].
-constexpr double lerp(double x, Point2d const& left_point, Point2d const& right_point);
+constexpr double lerp(double x, Point2d const& left_point, Point2d const& right_point) {
+    BOOST_VERIFY(left_point[0] <= x && x <= right_point[0]);
+    return left_point[1] + (x - left_point[0]) * (right_point[1] - left_point[1]) /
+                               (right_point[0] - left_point[0]);
+}
 
 } // namespace cherry_blazer::util
 
