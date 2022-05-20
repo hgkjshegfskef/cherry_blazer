35fe883e79e7d9e1daf91fa7b83735a887682c76
hgkjshegfskef <jyhgekyfbkjsyebf@protonmail.ch>
2022-05-20 22:58:14 +0200

    generate animation
    use 'convert -delay 10 -loop 0 image*.ppm animation.gif'

--- src/clock/generate.cc
+++ src/clock/generate.cc
@@ -10,8 +10,6 @@
 #include <fmt/core.h>
 
 #include <cerrno>
-#include <cmath>
-#include <iomanip>
 #include <iostream>
 #include <numbers>
 #include <ostream>
@@ -45,21 +43,48 @@ int main() {
     constexpr auto D = decltype(clock_origin)::size + 1;
     constexpr auto radius = 20.;
     constexpr auto canvas_size = radius * 2.5;
+    constexpr auto offset = canvas_size / 2;
 
-    Canvas canvas{canvas_size, canvas_size};
+    Canvas canvas{canvas_size + 1, canvas_size + 1};
     Color const white{255., 255., 255.};
-    auto const offset = double(canvas.width()) / 2;
+    Color const green{0., 255., 0.};
 
     auto const translation_matrix{Matrix<T, D, D>::translation(Matrix{0., -1., 0.})};
     auto const scaling_matrix{Matrix<T, D, D>::scaling(Matrix{radius, radius, radius})};
 
-    for (auto i{1U}; i <= 12; ++i) {
-        auto const point = Matrix<T, D, D>::rotation(Axis::Z, i * pi_v<T> / 6) *
-                           scaling_matrix * translation_matrix * clock_origin;
-        auto const x = point[0] + offset;
-        auto const y = point[1] + offset;
-        fmt::print("{:02d}: x: {:<5.1f} y: {:<5.1f}\n", i, x, y);
-        canvas(x, y) = white;
+    decltype(clock_origin) last_point;
+    for (auto i{0U}; i <= 12; ++i) {
+        auto const point = Matrix<T, D, D>::rotation(Axis::Z, i * pi_v<T> / 6) * scaling_matrix *
+                           translation_matrix * clock_origin;
+        if (i != 0) {
+            auto const x = point[0] + offset;
+            auto const y = point[1] + offset;
+            fmt::print("{:02d}: x: {:<7.3f} y: {:<7.3f}\n", i, x, y);
+            canvas(x, y) = white;
+            if (i != 1) {
+                canvas(last_point[0] + offset, last_point[1] + offset) = green;
+            }
+            last_point = point;
+        }
+
+        std::ofstream image_file;
+        try {
+            image_file = open_file("image" + std::to_string(i) + ".ppm");
+        } catch (std::system_error const& e) {
+            std::cerr << e.what() << " (" << e.code() << ")" << std::endl;
+            std::terminate();
+        }
+
+        std::string const image_contents = canvas.as_ppm();
+        image_file.write(image_contents.data(), long(image_contents.size()));
+    }
+
+    for (auto i{13U}; i <= 24; ++i) {
+        canvas(last_point[0] + offset, last_point[1] + offset) = green;
+        auto const point = Matrix<T, D, D>::rotation(Axis::Z, i * pi_v<T> / 6) * scaling_matrix *
+                           translation_matrix * clock_origin;
+        canvas(point[0] + offset, point[1] + offset) = white;
+        last_point = point;
 
         std::ofstream image_file;
         try {
